ab711cb0f176f250a6195ba65f833da38a109cfa
ndang <ndang219@gmail.com>
2025-03-25 19:29:41 +0000

    feat: update database connection to use postgres client with drizzle ORM

--- lib/db/index.ts
+++ lib/db/index.ts
@@ -1,4 +1,7 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import postgres from "postgres";
+
 import { env } from "@/env/server";
 
-export const db = drizzle(env.DATABASE_URL);
+const client = postgres(env.DATABASE_URL!);
+export const db = drizzle({ client });
